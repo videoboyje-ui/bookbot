c59c37d0ac666df78189b1b13d6b8050c3f6ea55
videoboyje-ui <videoboyje@gmail.com>
2025-10-31 22:36:38 -0400

    Main function and program was updated to allow different books to be input to the program for analysis.
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from stats import get_book_text, get_word_count, char_count, sort_char_analysis
+import sys
 
 def main():
-    book_file_path = "books/frankenstein.txt"
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    
+    book_file_path = sys.argv[1]
     
     # print total number of words in book
     num_words = get_word_count(get_book_text(book_file_path))
